57db6441f34b1aa8f6472e99d55f4d7e238684c4
Matheus07Henrique <matheushenriquefs07@gmail.com>
2022-09-25 16:37:54 -0300

    Adicionando o componente Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import { Header } from './components/Header';
+import './styles/global.css';
+import './styles/header.css';
 
 const App = (props: any) => {
 
   return (
     <div className="App">
-      <h1>Melhor Idade - IFRN CNAT</h1>
-      <p>Em construção...</p>
+      <Header></Header>
     </div>
   )
 };
---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+export function Header() {
+    return(
+        <header>
+            <div className="menu-icon">
+                <img src="home.png" alt="" />
+            </div>
+            <div className="menu-icon">
+                <img src="menu-sanduiche.png" alt="" />
+            </div>
+        </header>
+    )
+}
